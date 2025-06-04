f6c33467d1dbd26b8af2801f977060da5c61f821
pasha <pashatat47@gmail.com>
2025-06-04 23:15:01 +0300

    body validation schema fixed

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -42,5 +42,5 @@ export const updateStudentSchema = Joi.object({
     'any.required': 'Email is required',
   }),
   isFavourite: Joi.boolean(),
-  contactsType: Joi.string().valid('work', 'home', 'personal'),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
 });
